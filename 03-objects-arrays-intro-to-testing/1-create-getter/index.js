7dcaf2ffcbca2f377f088a6fef888c5b09975bd8
Mikhail Shkarin <blackgard2@yandex.ru>
2024-01-31 21:39:43 +0900

    objects_arrays_getter_revu

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const getterPath = path.split('.');
+  if (!getterPath) return;
+  return (obj) => {
+    for (let path of getterPath) {
+      if (!obj[path]) return;
+      obj = obj[path];
+    }
+    return obj;
+  }
 }
